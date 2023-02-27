f6703311ce63ba7f9c9472db07af5a5c317bd22e
mattgawarecki <mattgawarecki@users.noreply.github.com>
2023-02-27 13:43:58 +0000

    Revise Jetpack connection agreement text to comply with our User Agreement (#28403)
    Co-authored-by: Matt Gawarecki <matt.gawarecki@automattic.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/4282877269

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-polyfill', 'wp-url'), 'version' => '07a12283887a8bc9d150');
+<?php return array('dependencies' => array('lodash', 'react', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-polyfill', 'wp-url'), 'version' => 'c56d79afb755a413d12d');
