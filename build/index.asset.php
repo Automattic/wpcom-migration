384ac96dd44923ac484bed8cff2ec801ebcc1aa2
samiff <samiff@users.noreply.github.com>
2023-07-17 18:27:51 +0000

    Fix sonar qube issues (#31684)
    * fix issues
    
    * changelog
    
    * update 'for' loop
    
    * fix changelog versions
    
    ---------
    
    Co-authored-by: Samiff <samiff@users.noreply.github.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/5579043531

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '2d8ad9d9085f606fd1d3');
+<?php return array('dependencies' => array('lodash', 'react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'd344f65f112999da9d53');
