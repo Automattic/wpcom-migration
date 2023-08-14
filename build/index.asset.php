28caf676536eb666b22f8dcb37f20327b7ad511d
Initsogar <Initsogar@users.noreply.github.com>
2023-08-14 12:30:13 +0000

    Dashboard: Add backup undo feature (#32442)
    * Add endpoint to fetch backup undo event
    
    * changelog
    
    * Add backup undo event API
    
    * Add actions/reducers to handle undo event fetch
    
    * Refactor undo event reducer
    
    * Add component to query backup undo event
    
    * Add backup undo feature in backup card
    
    * Enable undo feature for WoA
    
    * Update projects versions
    
    * Move undo-event API to backup REST controller
    
    * Fetch undo event only when rewind status is active
    
    * Adjust track events for backup undo view and click
    
    * Update projects/plugins/jetpack/_inc/client/components/data/query-backup-undo-event/index.jsx
    
    Co-authored-by: Rustam Shaikh <rustam.shaikh@automattic.com>
    
    ---------
    
    Co-authored-by: Rustam Shaikh <rustam.shaikh@automattic.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/5855480389

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '27de01dfb3df70b4e0ff');
+<?php return array('dependencies' => array('lodash', 'react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '17d46c69083a97f4ee9e');
