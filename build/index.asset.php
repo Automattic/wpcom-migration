335c029991507e0a5ae4691596da7e259aca2e2e
andrii-lysenko <andrii-lysenko@users.noreply.github.com>
2023-12-29 17:41:09 +0000

    Modules store fill initial state (#34794)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/7358597973

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '119ecbb06d11497f7731');
+<?php return array('dependencies' => array('lodash', 'react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'b84980de85c30897b451');
