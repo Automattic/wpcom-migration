ea5c3a1370f2f9265b71f6796f90ad7f9e47cd4c
andrii-lysenko <andrii-lysenko@users.noreply.github.com>
2024-01-02 15:16:41 +0000

    In modules store make module updating more precise (#34801)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/7387241168

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'b84980de85c30897b451');
+<?php return array('dependencies' => array('lodash', 'react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'c843076c020ba440bd05');
