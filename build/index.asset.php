02a74ceeb5d3e9e566fd2f406dafbbe0c8709c14
monsieur-z <monsieur-z@users.noreply.github.com>
2024-03-25 13:44:11 +0000

    Jetpack Connection: refactor connection screen components to use TS (#36508)
    * Jetpack Connection: use TS for base components
    
    * Update pnpm-lock.yaml
    
    * Move error message from comp to function
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/8420893382

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '06a923c5996cb3cd2866');
+<?php return array('dependencies' => array('lodash', 'react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '7402bcdd31b833f7a1c7');
