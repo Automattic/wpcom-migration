4dc4609f16bf80bddcd03be30862b70ec0198336
CodeyGuyDylan <CodeyGuyDylan@users.noreply.github.com>
2024-04-08 16:56:07 +0000

    Add/from property to connection call (#36741)
    * Add from property to connection call
    
    * changelog
    
    * Fix partner coupon register test
    
    * changelog
    
    * Fixup project versions
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/8603701175

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '3942e269b07162d2c2ad');
+<?php return array('dependencies' => array('lodash', 'react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '30fcd8f92f190dad3be3');
