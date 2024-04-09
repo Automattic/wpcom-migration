e07c40379722a1853a4688d31375d7ce0411f117
nateweller <nateweller@users.noreply.github.com>
2024-04-09 00:51:40 +0000

    A4A Plugin: Add ConnectedCard component for site connected state (#36747)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/8608521609

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '30fcd8f92f190dad3be3');
+<?php return array('dependencies' => array('lodash', 'react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '76116f3d3362ff274ec0');
