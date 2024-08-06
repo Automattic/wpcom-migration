d4192921cc2d3e4e45c806e983f03f5d2c3a38c1
danluu <danluu@users.noreply.github.com>
2024-08-06 19:30:02 +0000

    Lossless image optimization, part 2 (#38573)
    This is a continuation of #38398, broken into multiple parts in an attempt to avoid wpcom's 20 MB change limit. Please see #38398 for more details.
    
    This change contains image optimizations for most of projects/plugins/
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/10272568024
    
    Upstream-Ref: Automattic/jetpack@168016f0d892775aabe3335fde625d5e81432afa

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -32,6 +32,7 @@ This is an alpha version! The changes listed here are not final.
 
 ### Fixed
 - Backup: change some error messages to not trigger security scanners
+- Lossless image optimization of images in projects/plugins [subdirectories from a* through social]
 - Updated package dependencies.
 - Write helper script to ABSPATH by default, just like we did before
 
--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'd944b7b9cb3f463575d1');
+<?php return array('dependencies' => array('lodash', 'react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '68cca343a24b6dad021f');
